9738d9310cb52b187503650c6372f144ccdcd1b7
itadmin <itadmin@class.hsd1.wa.comcast.net>
2019-10-28 23:48:13 -0700

    udpate

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ function buildQueryURL() {
   return queryURL;
 }
 
+function buildQueryURL2(city) {
+  var queryParams = city;
+  console.log("queryParams: " + queryParams);
+  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + queryParams + "&units=imperial&APPID=694e9301cf58974cd061591a93c6ebf3";
+  console.log(queryURL);
+  return queryURL;
+}
+
 function buildQueryURL5day() {
   var queryParams = $("#search-term").val();
   console.log("queryParams: " + queryParams);
@@ -15,8 +23,22 @@ function buildQueryURL5day() {
   return queryURL;
 }
 
-function clear() {
-  $("#article-section").empty();
+function buildQueryURL5day2(city) {
+  var queryParams = city;
+  console.log("queryParams: " + queryParams);
+  var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + queryParams + "&units=imperial&APPID=694e9301cf58974cd061591a93c6ebf3";
+  console.log(queryURL);
+  return queryURL;
+}
+
+
+
+function clearTop() {
+  $("#top").empty();
+}
+
+function clearBottom() {
+  $("#bottom").empty();
 }
 
 function updateCurrentWeather(currentweatherdata) {
@@ -25,25 +47,12 @@ function updateCurrentWeather(currentweatherdata) {
   console.log(currentweatherdata);
   console.log("----------------Current Weather--------------------");
 
-    // Create the  list group to contain the articles and add the article content for each
-    var $printTop0 = $("<div>");
-    var $printTop1 = $("<div>");
-    var $printTop2 = $("<div>");
-    var $printTop3 = $("<div>");
-    //$printTop.addClass("list-group");
-
-    // Add the newly created element to the DOM
-    $("#top").append($printTop0);
-    $("#top").append($printTop1);
-    $("#top").append($printTop2);
-    $("#top").append($printTop3);
-
-    // Add data to the elements
-    $printTop0.append(currentweatherdata.name);
-    $printTop1.append(currentweatherdata.main.temp);
-    $printTop2.append(currentweatherdata.main.humidity);
-    $printTop3.append(currentweatherdata.wind.speed);
-    console.log("in update function" + currentweatherdata.main.temp);
+  // Add data to the elements
+  $("#city").html("<h2>" + currentweatherdata.name + " " + moment().format('L') + "</h2>");
+  $("#weather").html(currentweatherdata.weather.main);
+  $("#temp").html(currentweatherdata.main.temp + " °F");
+  $("#humidity").html(currentweatherdata.main.humidity + " %");
+  $("#wind").html(currentweatherdata.wind.speed + " MPH");
 
 }
 
@@ -52,30 +61,24 @@ function update5Day(data) {
   // Log the Current Weather Data to console, where it will show up as an object
   console.log(data);
   console.log("----------------5 Day--------------------");
-  
-  for (var i = 1; i <= 5; i++) {
-    var $printrow0 = $("<div>");
-    var $printrow1 = $("<div>");
-    var $printrow2 = $("<div>");
-    var $printrow3 = $("<img>");
 
-   var iconid = data.list[i].weather[i].icon;
-    // console.log("iconid "+data.list[i].weather[i].icon);
-    // var iconurl = "http://openweathermap.org/img/wn/"+data.list[i].weather[i].icon+"@2x.png";
-    $printrow3.attr("src","http://openweathermap.org/img/wn/"+iconid+"@2x.png");
-    
-    $("#bottom").append($printrow0);
-    $("#bottom").append($printrow1);
-    $("#bottom").append($printrow2);
-    $("#bottom").append($printrow3);
+    $(".fiveDay").empty();
 
-    $printrow0.append("day "+i+" Date "+data.list[i].dt_txt);
-    $printrow1.append("day "+i+" temp "+data.list[i].main.temp);
-    $printrow2.append("day "+i+" humidity "+data.list[i].main.humidity);
-  
-    
-  }
+    for (var i = 1; i <= 5; i++) {
+      var date = moment().add(i + 1, 'days').format("M/D/YYYY");
 
+      $("<div>", {
+
+        html: "<h6>" + date + "</h6>" + "<br>" +
+          "Temp : " + data.list[i].main.temp + " °F" + "<br>" +
+          "Humidity : " + data.list[i].main.humidity + " %",
+
+        id: "fiveBox",
+        appendTo: ".fiveDay"
+      });
+    }
+
+  //}
 }
 
 
@@ -88,58 +91,46 @@ $("#run-search").on("click", function (event) {
   // This way we can hit enter on the keyboard and it registers the search
   // (in addition to clicks). Prevents the page from reloading on form submit.
   event.preventDefault();
-
+  //Grab Search Data
+  var searchVal = $("#search-term").val();
   // Empty the region associated with the articles
   //clear();
-
   // Build the query URL for the ajax request to the NYT API
   var queryURL = buildQueryURL();
   var queryURL5day = buildQueryURL5day();
-
   // Make the AJAX request to the API - GETs the JSON data at the queryURL.
-  // The data then gets passed as an argument to the updatePage function
+  // The data then gets passed as an argument to the updateCurrentWeather function
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(updateCurrentWeather);
-
+  // Make the AJAX request to the API - GETs the JSON data at the queryURL.
+  // The data then gets passed as an argument to the update5Day function
   $.ajax({
     url: queryURL5day,
     method: "GET"
   }).then(update5Day);
 
+  //APPEND CLICKED SEARCHES BELOW
+  $("<button class='searchbutton'>" + searchVal + "</button>").appendTo(".searches");
+  $("<br>").appendTo(".searches");
+
+  //get value of search history button clicked and build new query and call functions again
+  $(".searchbutton").on("click", function (event) {
+    var buttoncity = $(this).text();
+    var queryURL2 = buildQueryURL2(buttoncity);
+    $.ajax({
+      url: queryURL2,
+      method: "GET"
+    }).then(updateCurrentWeather);
+    var queryURL5day2 = buildQueryURL5day2(buttoncity);
+    $.ajax({
+      url: queryURL5day2,
+      method: "GET"
+    }).then(update5Day);
+  })
 
 });
 
-//  .on("click") function associated with the clear button
-//$("#clear-all").on("click", clear);
 
-// $.ajax({
-//   url: queryURL,
-//   method: "GET"
-// }).then(function (response) {
-//   var temp = response.main.temp;
-//   var row = $("")
-//   var printleft = $("#left");
-//   printleft.append(temp);
 
-//   console.log(response);
-//   console.log("Temperature " + response.main.temp);
-//   console.log("Humidity " + response.main.humidity);
-//   console.log("Wind Speed " + response.wind.speed);
-//   console.log("Icon " + response.weather[0].icon);
-//   console.log(response.coord.lon);
-//   console.log(response.coord.lat);
-// });
-
-// var queryURL5day = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&APPID=694e9301cf58974cd061591a93c6ebf3";
-
-// $.ajax({
-//   url: queryURL5day,
-//   method: "GET"
-// }).then(function (response) {
-
-//   console.log(response.list[0].weather[0].icon);
-//   console.log(response.list[0].main.temp);
-//   console.log(response.list[0].main.humidity);
-// });
